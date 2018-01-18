e0e838f176fea7def88b73f91309ca5afb93ea24
sayantikabanik <sayantikabanik122@gmail.com>
2018-01-18 12:04:00 +0530

    code blocks for pidcontroller

---
+++ pidcontroller.py
@@ -0,0 +1,26 @@
+'''HARIOMHARIBOLJAIMATAJIPITAJIKIJAIJAI'''
+
+
+class PIDController:
+    def __init__(self, P, I, D):
+        self.KP = P
+        self.KI = I
+        self.KD = D
+        self.target = 0
+
+        self.lastError = 0
+        self.integrator = 0
+
+    def setTarget(self, newTarget):
+        self.target = newTarget
+        self.integrator = 0
+
+    def step(self, currentValue):
+        error = currentValue - self.target
+
+        output = (self.KP * error + self.KI * self.integrator + self.KD * (error - self.lastError))
+
+        self.lastError = error
+        self.integrator += error
+
+return output
